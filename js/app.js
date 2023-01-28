eb8e95897d5d16d889a8e4c4e70a3a6feefc41cc
Sanal MG <sanalmg@outlook.com>
2023-01-28 19:51:42 +0530

    Added initial files

---
+++ js/app.js
@@ -0,0 +1,20 @@
+const menuButton = document.querySelector('.menu-btn');
+const menuCloseButton = document.querySelector('.menu-close-btn')
+const sidebarMenu = document.querySelector('.collections')
+
+
+menuButton.addEventListener('click', (e) => {
+
+    e.preventDefault();
+    if (!sidebarMenu.classList.contains('on')) {
+        sidebarMenu.classList.add('on');
+    }
+
+});
+
+menuCloseButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    if (sidebarMenu.classList.contains('on')) {
+        sidebarMenu.classList.remove('on');
+    }
+});
