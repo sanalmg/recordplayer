7acfbecbebc57ff72f8a13a50fbe2c6fe12a9224
Sanal MG <sanal.mg@finastra.com>
2023-02-03 21:14:51 +0530

    fixed player height issue

--- js/app.js
+++ js/app.js
@@ -1,17 +1,17 @@
+/* Global variables */
 const menuButton = document.querySelector('.menu-btn');
 const menuCloseButton = document.querySelector('.menu-close-btn')
 const sidebarMenu = document.querySelector('.collections')
 
 
-menuButton.addEventListener('click', (e) => {
+/* Sidebar menu function */
 
+menuButton.addEventListener('click', (e) => {
     e.preventDefault();
     if (!sidebarMenu.classList.contains('on')) {
         sidebarMenu.classList.add('on');
     }
-
 });
-
 menuCloseButton.addEventListener('click', (e) => {
     e.preventDefault();
     if (sidebarMenu.classList.contains('on')) {
